a7acd8c802e305b2d0ddcd6142fc1ca3f2479c95
Jesse A. Tov <jesse.tov@gmail.com>
2018-02-26 18:21:51 -0600

    Need to test maps.

--- src/weak_unordered_map.h
+++ src/weak_unordered_map.h
@@ -1,6 +1,7 @@
 #pragma once
 
 #include "detail/rh_weak_hash_table.h"
+#include "detail/weak_unordered_map_base.h"
 #include "weak_traits.h"
 #include "weak_pair.h"
 
@@ -25,52 +26,6 @@ public:
 template<class Key, class Value, class Hash, class KeyEqual, class Allocator>
 void swap(weak_unordered_map<Key, Value, Hash, KeyEqual, Allocator>& a,
           weak_unordered_map<Key, Value, Hash, KeyEqual, Allocator>& b)
-{
-    a.swap(b);
-}
-
-template<class Key, class Value,
-         class Hash = std::hash<Key>,
-         class KeyEqual = std::equal_to<Key>,
-         class Allocator = std::allocator<weak_key_pair<Key, Value>>>
-class weak_key_unordered_map
-        : public detail::weak_unordered_map_base<weak_key_pair < Key, Value>,
-          Hash, KeyEqual, Allocator>
-{
-    using BaseClass = detail::weak_unordered_map_base <weak_key_pair<Key, Value>,
-                                                       Hash, KeyEqual, Allocator>;
-public:
-    using BaseClass::weak_unordered_map_base;
-};
-
-template<class Key, class Value, class Hash, class KeyEqual, class Allocator>
-void swap(weak_key_unordered_map <Key, Value, Hash, KeyEqual, Allocator>& a,
-          weak_key_unordered_map <Key, Value, Hash, KeyEqual, Allocator>& b)
-{
-    a.swap(b);
-}
-
-template<class Key, class Value,
-        class Hash = std::hash<Key>,
-        class KeyEqual = std::equal_to<Key>,
-        class Allocator = std::allocator<weak_value_pair < Key, Value>> >
-
-class weak_value_unordered_map
-        : public weak_unordered_map_base<weak_value_pair < Key, Value>,
-          Hash, KeyEqual, Allocator
-
->
-{
-using BaseClass = weak_unordered_map_base <weak_value_pair<Key, Value>,
-Hash, KeyEqual,
-Allocator>;
-public:
-using BaseClass::weak_unordered_map_base;
-};
-
-template<class Key, class Value, class Hash, class KeyEqual, class Allocator>
-void swap(weak_value_unordered_map<Key, Value, Hash, KeyEqual, Allocator>& a,
-          weak_value_unordered_map<Key, Value, Hash, KeyEqual, Allocator>& b)
 {
     a.swap(b);
 }
---
+++ test/weak_unordered_map_test.cpp
@@ -0,0 +1,15 @@
+#include "weak_unordered_map.h"
+#include "weak_key_unordered_map.h"
+#include "weak_value_unordered_map.h"
+
+#include <catch.hpp>
+#include <string>
+
+using namespace weak;
+using namespace std;
+
+TEST_CASE("weak_key_unordered_map")
+{
+//    weak_key_unordered_map<string, int> map;
+
+}
