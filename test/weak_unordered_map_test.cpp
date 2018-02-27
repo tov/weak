dc3b83f743295c9c47c1283499d0b4caf971b0ea
Jesse A. Tov <jesse.tov@gmail.com>
2018-02-26 18:52:21 -0600

    More map tests.

--- test/weak_unordered_map_test.cpp
+++ test/weak_unordered_map_test.cpp
@@ -15,16 +15,35 @@ TEST_CASE("weak_key_unordered_map")
 
     auto hello = make_shared<string>("hello");
     map.insert({hello, 5});
-
     CHECK( map.find("hello") != map.end() );
+
+    hello = nullptr;
+    CHECK( map.find("hello") == map.end() );
 }
 
 TEST_CASE("weak_value_unordered_map")
 {
     weak_value_unordered_map<string, int> map;
+    CHECK( map.find("hello") == map.end() );
+
+    auto five = make_shared<int>(5);
+    map.insert({"hello", five});
+    CHECK( map.find("hello") != map.end() );
+
+    five = nullptr;
+    CHECK( map.find("hello") == map.end() );
 }
 
 TEST_CASE("weak_unordered_map")
 {
     weak_unordered_map<string, int> map;
+    CHECK( map.find("hello") == map.end() );
+
+    auto hello = make_shared<string>("hello");
+    auto five = make_shared<int>(5);
+    map.insert({hello, five});
+    CHECK( map.find("hello") != map.end() );
+
+    five = nullptr;
+    CHECK( map.find("hello") == map.end() );
 }
