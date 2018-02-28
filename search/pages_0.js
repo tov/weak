8f52bba934fbc0f55754d123f18ed96246d83bc9
Jesse A. Tov <jesse.tov@gmail.com>
2018-02-27 23:46:40 -0600

    Update documentation

--- search/pages_0.js
+++ search/pages_0.js
@@ -1,4 +1,4 @@
 var searchData=
 [
-  ['weak_3a_20weak_20hash_20tables_20for_20c_2b_2b',['weak: weak hash tables for C++',['../index.html',1,'']]]
+  ['weak_3a_20weak_20hash_20tables_20for_20c_2b_2b17',['weak: weak hash tables for C++17',['../index.html',1,'']]]
 ];
