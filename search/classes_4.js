16abbabadb69ccba2c6a5cbdfa3e5ef178529fdf
Jesse A. Tov <jesse.tov@gmail.com>
2018-02-27 23:58:09 -0600

    Update documentation

--- search/classes_4.js
+++ search/classes_4.js
@@ -2,9 +2,9 @@ var searchData=
 [
   ['weak_5fhash_5ftable_5fbase',['weak_hash_table_base',['../classweak_1_1weak__hash__table__base.html',1,'weak']]],
   ['weak_5fhash_5ftable_5fbase_3c_20std_3a_3aweak_5fptr_3c_20const_20key_20_3e_2c_20hash_2c_20keyequal_2c_20allocator_20_3e',['weak_hash_table_base&lt; std::weak_ptr&lt; const Key &gt;, Hash, KeyEqual, Allocator &gt;',['../classweak_1_1weak__hash__table__base.html',1,'weak']]],
-  ['weak_5fhash_5ftable_5fbase_3c_20weak_5fkey_5fpair_3c_20key_2c_20value_20_3e_2c_20hash_2c_20keyequal_2c_20allocator_20_3e',['weak_hash_table_base&lt; weak_key_pair&lt; Key, Value &gt;, Hash, KeyEqual, Allocator &gt;',['../classweak_1_1weak__hash__table__base.html',1,'weak']]],
-  ['weak_5fhash_5ftable_5fbase_3c_20weak_5fvalue_5fpair_3c_20key_2c_20value_20_3e_2c_20hash_2c_20keyequal_2c_20allocator_20_3e',['weak_hash_table_base&lt; weak_value_pair&lt; Key, Value &gt;, Hash, KeyEqual, Allocator &gt;',['../classweak_1_1weak__hash__table__base.html',1,'weak']]],
-  ['weak_5fhash_5ftable_5fbase_3c_20weak_5fweak_5fpair_3c_20key_2c_20value_20_3e_2c_20hash_2c_20keyequal_2c_20allocator_20_3e',['weak_hash_table_base&lt; weak_weak_pair&lt; Key, Value &gt;, Hash, KeyEqual, Allocator &gt;',['../classweak_1_1weak__hash__table__base.html',1,'weak']]],
+  ['weak_5fhash_5ftable_5fbase_3c_20weak_5fkey_5fpair_3c_20key_2c_20t_20_3e_2c_20hash_2c_20keyequal_2c_20allocator_20_3e',['weak_hash_table_base&lt; weak_key_pair&lt; Key, T &gt;, Hash, KeyEqual, Allocator &gt;',['../classweak_1_1weak__hash__table__base.html',1,'weak']]],
+  ['weak_5fhash_5ftable_5fbase_3c_20weak_5fvalue_5fpair_3c_20key_2c_20t_20_3e_2c_20hash_2c_20keyequal_2c_20allocator_20_3e',['weak_hash_table_base&lt; weak_value_pair&lt; Key, T &gt;, Hash, KeyEqual, Allocator &gt;',['../classweak_1_1weak__hash__table__base.html',1,'weak']]],
+  ['weak_5fhash_5ftable_5fbase_3c_20weak_5fweak_5fpair_3c_20key_2c_20t_20_3e_2c_20hash_2c_20keyequal_2c_20allocator_20_3e',['weak_hash_table_base&lt; weak_weak_pair&lt; Key, T &gt;, Hash, KeyEqual, Allocator &gt;',['../classweak_1_1weak__hash__table__base.html',1,'weak']]],
   ['weak_5fkey_5fpair',['weak_key_pair',['../structweak_1_1weak__key__pair.html',1,'weak']]],
   ['weak_5fkey_5funordered_5fmap',['weak_key_unordered_map',['../classweak_1_1weak__key__unordered__map.html',1,'weak']]],
   ['weak_5ftraits',['weak_traits',['../structweak_1_1weak__traits.html',1,'weak']]],
